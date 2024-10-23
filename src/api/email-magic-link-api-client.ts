daae0ca9285457e8150105f0a5812ad3b7e80818
Hamish Meikle <hwhmeikle@gmail.com>
2024-10-23 12:22:59 +1100

    Add email magic link verification

---
+++ src/api/email-magic-link-api-client.ts
@@ -0,0 +1,71 @@
+import {ChallengeResponse, EnrollResponse} from "../types";
+import {buildHeaders, handleTokenExpired} from "./helpers";
+import {CheckVerificationStatusResponse} from "./types/email-magic-link";
+import {ApiClientOptions, ErrorResponse} from "./types/shared";
+
+export class EmailMagicLinkApiClient {
+  tenantId: string;
+  baseUrl: string;
+  onTokenExpired?: () => void;
+
+  constructor({baseUrl, tenantId, onTokenExpired}: ApiClientOptions) {
+    this.tenantId = tenantId;
+    this.baseUrl = baseUrl;
+    this.onTokenExpired = onTokenExpired;
+  }
+
+  async enroll({token, email}: {token: string; email: string}): Promise<EnrollResponse | ErrorResponse> {
+    const body = {email};
+
+    const response = await fetch(`${this.baseUrl}/client/user-authenticators/email-magic-link`, {
+      method: "POST",
+      headers: buildHeaders({token, tenantId: this.tenantId}),
+      body: JSON.stringify(body),
+    });
+
+    const responseJson = await response.json();
+
+    handleTokenExpired({response: responseJson, onTokenExpired: this.onTokenExpired});
+
+    return responseJson;
+  }
+
+  async challenge({token}: {token: string}): Promise<ChallengeResponse | ErrorResponse> {
+    const response = await fetch(`${this.baseUrl}/client/challenge/email-magic-link`, {
+      method: "POST",
+      headers: buildHeaders({token, tenantId: this.tenantId}),
+    });
+
+    const responseJson = await response.json();
+
+    handleTokenExpired({response: responseJson, onTokenExpired: this.onTokenExpired});
+
+    return responseJson;
+  }
+
+  async checkVerificationStatus({token}: {token: string}): Promise<CheckVerificationStatusResponse | ErrorResponse> {
+    const pollVerificationStatus = async (): Promise<CheckVerificationStatusResponse | ErrorResponse> => {
+      const response = await fetch(`${this.baseUrl}/client/verify/email-magic-link/finalize`, {
+        method: "POST",
+        headers: buildHeaders({token, tenantId: this.tenantId}),
+        body: JSON.stringify({}),
+      });
+
+      const responseJson = await response.json();
+
+      handleTokenExpired({response: responseJson, onTokenExpired: this.onTokenExpired});
+
+      if (responseJson.isVerified) {
+        return responseJson;
+      } else {
+        return new Promise((resolve) => {
+          setTimeout(async () => {
+            resolve(await pollVerificationStatus());
+          }, 1000);
+        });
+      }
+    };
+
+    return await pollVerificationStatus();
+  }
+}
---
+++ src/email-magic-link.ts
@@ -0,0 +1,58 @@
+import {EmailMagicLinkApiClient} from "./api/email-magic-link-api-client";
+import {CheckVerificationStatusResponse} from "./api/types/email-magic-link";
+import {handleApiResponse} from "./helpers";
+import {TokenCache} from "./token-cache";
+import {AuthsignalResponse, ChallengeResponse, EnrollResponse} from "./types";
+
+type EmailMagicLinkOptions = {
+  baseUrl: string;
+  tenantId: string;
+  onTokenExpired?: () => void;
+};
+
+type EnrollParams = {
+  email: string;
+};
+
+export class EmailMagicLink {
+  private api: EmailMagicLinkApiClient;
+  private cache = TokenCache.shared;
+
+  constructor({baseUrl, tenantId, onTokenExpired}: EmailMagicLinkOptions) {
+    this.api = new EmailMagicLinkApiClient({baseUrl, tenantId, onTokenExpired});
+  }
+
+  async enroll({email}: EnrollParams): Promise<AuthsignalResponse<EnrollResponse>> {
+    if (!this.cache.token) {
+      return this.cache.handleTokenNotSetError();
+    }
+
+    const response = await this.api.enroll({token: this.cache.token, email});
+
+    return handleApiResponse(response);
+  }
+
+  async challenge(): Promise<AuthsignalResponse<ChallengeResponse>> {
+    if (!this.cache.token) {
+      return this.cache.handleTokenNotSetError();
+    }
+
+    const response = await this.api.challenge({token: this.cache.token});
+
+    return handleApiResponse(response);
+  }
+
+  async checkVerificationStatus(): Promise<AuthsignalResponse<CheckVerificationStatusResponse>> {
+    if (!this.cache.token) {
+      return this.cache.handleTokenNotSetError();
+    }
+
+    const response = await this.api.checkVerificationStatus({token: this.cache.token});
+
+    if ("accessToken" in response && response.accessToken) {
+      this.cache.token = response.accessToken;
+    }
+
+    return handleApiResponse(response);
+  }
+}
