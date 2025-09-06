ab7f85f33d23e38bf76d6a516acb572056880364
Aaron Zhong <aaronzhongnz@gmail.com>
2025-09-06 14:28:22 +1200

    Do not use cached token

--- src/digital-credential.ts
+++ src/digital-credential.ts
@@ -28,8 +28,6 @@ export class DigitalCredential {
   }
 
   async requestCredential(params?: RequestCredentialParams): Promise<AuthsignalResponse<RequestCredentialResponse>> {
-    const userToken = params?.token ?? this.cache.token;
-
     if (!(await this.browserSupportsDigitalCredential())) {
       throw new Error("Digital Credential API is not supported in this browser");
     }
@@ -41,7 +39,7 @@ export class DigitalCredential {
     }
 
     const optionsResponse = await this.api.presentationOptions({
-      token: userToken || undefined,
+      token: params?.token || undefined,
       challengeId: challengeResponse?.challengeId,
     });
 
@@ -51,44 +49,36 @@ export class DigitalCredential {
 
     const {dcapiOptions, challengeId} = optionsResponse;
 
-    try {
-      const credentialResponse = await navigator.credentials.get(dcapiOptions);
+    const credentialResponse = await navigator.credentials.get(dcapiOptions);
 
-      if (!credentialResponse) {
-        throw new Error("No credential was provided");
-      }
-
-      // @ts-expect-error - Digital Credential API is experimental
-      const {data} = credentialResponse;
-
-      const verifyResponse = await this.api.verifyPresentation({
-        token: userToken || undefined,
-        data,
-        nonce: dcapiOptions.digital?.requests?.[0]?.data?.nonce,
-        challengeId,
-      });
-
-      if ("error" in verifyResponse) {
-        return handleErrorResponse(verifyResponse);
-      }
-
-      if (verifyResponse.accessToken) {
-        this.cache.token = verifyResponse.accessToken;
-      }
-
-      return {
-        data: {
-          isVerified: verifyResponse.isVerified,
-          token: verifyResponse.accessToken,
-        },
-      };
-    } catch (error: unknown) {
-      if (error instanceof Error && error.name === "NotAllowedError") {
-        throw new Error("User cancelled the credential request");
-      }
-
-      throw error;
+    if (!credentialResponse) {
+      throw new Error("No credential was provided");
     }
+
+    // @ts-expect-error - Digital Credential API is experimental
+    const {data} = credentialResponse;
+
+    const verifyResponse = await this.api.verifyPresentation({
+      token: params?.token || undefined,
+      data,
+      nonce: dcapiOptions.digital?.requests?.[0]?.data?.nonce,
+      challengeId,
+    });
+
+    if ("error" in verifyResponse) {
+      return handleErrorResponse(verifyResponse);
+    }
+
+    if (verifyResponse.accessToken) {
+      this.cache.token = verifyResponse.accessToken;
+    }
+
+    return {
+      data: {
+        isVerified: verifyResponse.isVerified,
+        token: verifyResponse.accessToken,
+      },
+    };
   }
 
   async browserSupportsDigitalCredential(): Promise<boolean> {
